1a86e190b2d5d8c65b8f3e7730da77ee2acec3fd
vrk0ranit <ranitbag49@gmail.com>
2024-02-13 11:33:57 +0530

    fix refresh token validation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -186,7 +186,7 @@ const registerUser = asyncHandler ( async (req, res) => {
             throw new ApiError(401, "Invalid refresh token")
          }
    
-         if(incomingRefreshToken !== user?. refreshToken){
+         if(!incomingRefreshToken !== user?. refreshToken){
             throw new ApiError(401, "Refresh token is expired or used")
          }
            
